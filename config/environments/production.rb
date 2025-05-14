4496d82aeb5e1da624cac1133c4d077dc4d6dd80
ayu8008 <ayu@hksi.or.jp>
2025-05-14 15:19:55 +0900

    fixed config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
 
   # 手動で追記
-  config.hosts << "product-register-f0y6.onrender.com"
+  config.hosts << /.*\.onrender\.com/
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
